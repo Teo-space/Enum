1d661c0cf57c2f1b49d148234aa7b4c1fe0f7d01
b.a.n <barmin.andrey.nnov@gmail.com>
2024-06-02 20:31:50 +0300

    upd

---
+++ Enum.Test/Infrastructure/ApplicationContext.cs
@@ -0,0 +1,31 @@
+﻿using Enum.Test.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Enum.Test.Infrastructure;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<Order> Orders { get; set; }
+
+    public ApplicationContext()
+    {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("DataSource=file::memory:?cache=shared");
+        optionsBuilder.LogTo(Console.WriteLine, minimumLevel: Microsoft.Extensions.Logging.LogLevel.Information);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        modelBuilder.Entity<Order>()
+            .Property(x => x.Status)
+            .HasConversion(x => x.Key, x => OrderStatus.FromKey<OrderStatus>(x));
+    }
+}
--- Enum.Test/Program.cs
+++ Enum.Test/Program.cs
@@ -1,8 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System.ComponentModel.DataAnnotations;
-using System.Reflection;
+﻿using Enum.Test.Domain;
+using Enum.Test.Infrastructure;
 
 List<Order> orders = [];
+
 
 orders.Add(new Order
 {
@@ -17,63 +17,19 @@ orders.Add(new Order
     Status = OrderStatus.Done,
 });
 
+
 using var db = new ApplicationContext();
 
 db.Orders.AddRange(orders);
 db.SaveChanges();
 
-Console.WriteLine(db.Orders.Count().ToString(), ConsoleColor.DarkMagenta);
-Console.WriteLine(db.Orders.Count(x => x.Status > OrderStatus.Pending).ToString(), ConsoleColor.DarkMagenta);
+
+
+Console.WriteLine($"Total: {db.Orders.Count().ToString()}", ConsoleColor.DarkMagenta);
+
+Console.WriteLine($"Pending: {db.Orders.Count(x => x.Status > OrderStatus.Pending).ToString()}", ConsoleColor.DarkMagenta);
+
 
 var first = db.Orders.OrderBy(x => x.OrderId).First();
 Console.WriteLine(first);
 
-
-public class ApplicationContext : DbContext
-{
-    public DbSet<Order> Orders { get; set; }
-    public ApplicationContext()
-    {
-        Database.EnsureCreated();
-    }
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseSqlite("DataSource=file::memory:?cache=shared");
-        optionsBuilder.LogTo(Console.WriteLine, minimumLevel: Microsoft.Extensions.Logging.LogLevel.Information);
-    }
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
-
-        modelBuilder.Entity<Order>()
-            .Property(x => x.Status)
-            .HasConversion(x => x.Key, x => OrderStatus.FromKey<OrderStatus>(x));
-    }
-}
-
-
-public sealed record Order
-{
-    [Key]
-    public int OrderId { get; set; }
-
-    public DateTime Date { get; set; }
-
-    public OrderStatus Status { get; set; } = OrderStatus.Pending;
-}
-
-
-
-public sealed record OrderStatus : Enum<OrderStatus>
-{
-    protected OrderStatus(int Key, string Value) : base(Key, Value) { }
-
-    public static OrderStatus Pending => new OrderStatus(0, "Ожидание");
-
-    public static OrderStatus Done => new OrderStatus(1, "Завершен");
-
-    public static OrderStatus Canceled => new OrderStatus(10000, "Отменен");
-
-
-}
